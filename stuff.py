4882e99bb7786882c8288d27fadb034c764d0bc0
ecyehl <45824065+ecyehl@users.noreply.github.com>
2023-03-23 11:01:40 -0500

    aslfnsl

--- stuff.py
+++ stuff.py
@@ -1,1 +1,13 @@
-
+number = 0
+n2 = 0
+with open("usernames.txt") as the:
+    for line in the:
+        if line =="cultiris":
+            break
+        number+=1
+with open("passwords.txt") as the:
+    for line in the:
+        if n2 == number:
+            print(line)
+            break
+        n2+=1
